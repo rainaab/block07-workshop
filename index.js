1e2fda9f52a9009abdabd8ec8d4e467b0af365d2
Raina Bethea <rainabethea@gmail.com>
2023-10-05 13:46:10 -0400

    javascript code

---
+++ index.js
@@ -0,0 +1,16 @@
+//Create 3 number vaiables
+//use operators to make and equation that equals each number
+//make an alert that reads out each number
+
+// variable 1 
+let varOne = 100/10
+// variable 2
+let varTwo = (5*2) + 20
+// variable 3
+let varThree = (8 * 5) - 1
+
+// message for user 
+
+const userMessage = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: "
+
+// alert message 
